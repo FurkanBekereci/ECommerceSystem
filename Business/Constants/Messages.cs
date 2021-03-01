c241e633d6ed2f01bb8f8e249e45e4ff99484aeb
FurkanBekereci <frknbkrci@gmail.com>
2021-03-01 23:28:12 +0300

    Kurs ile aynı yere gelindi.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,6 +14,12 @@ namespace Business.Constants
         public static string ProductMaxCountOfCategory = "Bir kategoride en fazla 10 tane ürün olabilir.";
         public static string ProductNameAlreadyExist = "Aynı isimde bir ürün eklenemez.";
         public static string MaxCategoryCount = "Mevcut kategori sayısı 15'i geçtiği için ürün eklenemez.";
-
+        public static string AuthorizationDenied = "Bu işlem için yetkiniz bulunmuyor.";
+        public static string UserRegistered = "Kullanıcı kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Hatalı şifre.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut.";
+        public static string AccessTokenCreated = "Bu token zaten oluşturuldu.";
     }
 }
